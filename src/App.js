a48b4b7ff6a4bc4c10931172e32fca4d9e529c78
= <=>
2022-02-23 17:47:50 +0200

    lesson_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+  const dispacth = useDispatch()
+  const cash = useSelector(state => state.cash)
+
+  const addCash = (cash) => {
+    dispacth({type: "ADD_CASH", payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispacth({type: "GET_CASH", payload: cash})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'app'}>
+      <div style={{fontSize:'3rem'}}>{cash}</div>
+      <div style={{display:'flex'}}>
+        <button onClick={()=>addCash(Number(prompt()))}>Внести деньги</button>
+        <button onClick={()=>getCash(Number(prompt()))}>Снять деньги</button>
+      </div>      
     </div>
   );
 }
